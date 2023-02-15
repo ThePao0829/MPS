c3f5972c5723e960d3a8bdd8be64de53c0d73ab2
ThePao0829 <80174196+ThePao0829@users.noreply.github.com>
2023-02-15 15:06:57 +0100

    testAdded
    Anyadir los tests sin comprobar funcionamiento

--- src/test/java/org/example/PersonTest.java
+++ src/test/java/org/example/PersonTest.java
@@ -24,4 +24,68 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
 
+    Person person;
+
+    @BeforeEach
+    void setup(){
+        person = new Person();
+    }
+
+    @AfterEach
+    void shutdown(){
+        person = null;
+    }
+
+    @Test
+    public void ifNullName(){
+        assertThrows(RuntimeException.class, () -> person = new Person(null, 1, "Male"));
+    }
+    @Test
+    public void ifNullGender(){
+        assertThrows(RuntimeException.class, () -> person = new Person("Name", 1, null));
+    }
+    @Test
+    public void ifAgeMinusZero(){
+        assertThrows(RuntimeException.class, () -> person = new Person("Name", -5, "Male"));
+    }
+    @Test
+    public void ifErrorValueOfGender(){
+        assertThrows(RuntimeException.class, () -> person = new Person("Name", 19, "FMale"));
+    }
+    @Test
+    public void allValidValue(){
+        String expectedName = "Antonio";
+        String expectedGender = "Male";
+        int expectedAge = 21;
+        Person obtainedPerson = new Person(expectedName, expectedAge, expectedGender);
+
+        assertEquals(expectedName, obtainedPerson.name());
+        assertEquals(expectedAge, obtainedPerson.age());
+        assertEquals(expectedGender, obtainedPerson.gender());
+    }
+    @Test
+    public void emptyList(){
+        double[] expectedValue = {0,0};
+        List<Person> list = new ArrayList<>();
+        double[] obtainedValue = person.averageAgePerGender(list);
+        assertArrayEquals(expectedValue, obtainedValue);
+    }
+    @Test
+    public void averageOfList(){
+        double[] expectedValue = {3,8};
+        List<Person> list = new ArrayList<>();
+        list.add(new Person("1", 1, "Male"));
+        list.add(new Person("2", 2, "Male"));
+        list.add(new Person("3", 3, "Male"));
+        list.add(new Person("4", 4, "Male"));
+        list.add(new Person("5", 5, "Male"));
+        list.add(new Person("6", 6, "Female"));
+        list.add(new Person("7", 7, "Female"));
+        list.add(new Person("8", 8, "Female"));
+        list.add(new Person("9", 9, "Female"));
+        list.add(new Person("10", 10, "Female"));
+
+        double[] obtainedValue = person.averageAgePerGender(list);
+        assertArrayEquals(expectedValue, obtainedValue);
+    }
 }
